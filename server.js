5ddc6d531c6b7a8be59f39eef730fa4f81fde4ec
Samuel Sholib <samuelsholib12@gmail.com>
2021-11-29 17:18:04 -0800

    server js completed

--- server.js
+++ server.js
@@ -18,7 +18,8 @@ app.get('/api/notes', (req, res) => {
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
-app.get('/', (req, res) => {
+
+app.get('/notes', (req, res) => {
     res.sendFile(path.json(__dirname, './public/notes.html'));
 });
 
@@ -29,5 +30,43 @@ app.get('*', (req, res) => {
 function writeNotes(body, notesArray) {
     const newNotes = body;
 
+    if (!Array.isArray(notesArray))
+        notesArray = [];
+    body.id = notesArray[0]
+    notesArray[0]++;
+
+    notesArray.push(newNotes);
+    fs.writeFileSync(path.join(__dirname, './db/db.json'),
+        JSON.stringify(notesArray, null, 2)
+    );
+    return newNotes;
+}
+
+app.post('/api/notes', (req, res) => {
+    const newNote = writeNotes(req.body, writeNote);
+    res.json(newNote);
+});
+
+function deleteNote(id, notesArray) {
+    for (let i = 0; i < notesArray.length; i++) {
+        let note = notesArray[i];
+
+        if (note.id == id) {
+            notesArray.splice(i, 1);
+            fs.writeFileSync(path.join(__dirname, './db/db.json'),
+                JSON.stringify(notesArray, null, 2)
+            );
+            break;
+        }
+    }
 
 }
+
+app.delete('/api/notes/:id', (req, res) => {
+    deleteNote(req.params.id, writeNote);
+    res.json(true);
+});
+
+app.listen(PORT, () => {
+    console.log('generate');
+});
